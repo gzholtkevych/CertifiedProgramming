640205d739b256ffc110f5992cfcb1aeaefaa9a6
gzholtkevych <g.zholtkevych@gmail.com>
2023-10-12 16:39:35 +0300

    Update NatSorting.v

--- CoqScripts/NatSorting.v
+++ CoqScripts/NatSorting.v
@@ -9,8 +9,8 @@ Inductive sorted : list nat -> Prop :=
       sorted []
   | sort1 : (* будь-який одноелементний список є відсортованим                *)
       forall n, sorted [n]
-  | sortS : (* якщо в голову відсортованого списку додати число меше
-               за перший елемент списку, то отримаємо відсортований список    *)
+  | sortS : (* якщо в голову відсортованого списку додати число меше або рівне
+               першому елементу списку, то отримаємо відсортований список     *)
       forall n m lst, 
         n <= m -> sorted (m :: lst) -> sorted (n :: m :: lst).
 
@@ -33,7 +33,6 @@ Fixpoint occnum (n : nat) (lst : list nat) : nat :=
   | [] => 0
   | m :: lst' => if Nat.eq_dec n m then S (occnum n lst') else occnum n lst'
   end.
-
 Eval simpl in occnum 3 [].
 Eval simpl in occnum 3 [1; 3; 2; 5; 3].
 
@@ -54,11 +53,11 @@ Variables (lst1 lst2 lst3 : list nat) (n m : nat).
 
   Lemma same_transitivity : same lst1 lst2 -> same lst2 lst3 -> same lst1 lst3.
   Proof. unfold same. intros. rewrite <- H0. exact (H n0). Qed.
-  (* тобто відношенням схоєесті є еквівалентністю на спиках                   *)
+  (* тобто відношенням схожесті є еквівалентністю на спиках                   *)
 
   Lemma same_cons : same lst1 lst2 -> same (n :: lst1) (n :: lst2).
   (* якщо в голови схожих списків вставити одне й те саме число,
-     то торимаємо схожі списки                                                *)
+     то отримаємо схожі списки                                                *)
   Proof. unfold same. intros. simpl. rewrite H. reflexivity. Qed.
 
   Lemma same_permutation : 
@@ -75,9 +74,9 @@ End SameProperties.
   same_reflexivity same_symmetry same_transitivity 
   same_cons same_permutation : sortHDB.
 
-Definition SortSpec :=
+Definition SortCert :=
   (* алгоритм сортування є                                                    *)
-  { f  (* функцією, що перетворює список на список                            *)
+  { f : list nat -> list nat  (* функцією, що перетворює список на список     *)
     | (forall lst, same lst (f lst)) /\  (* зберігає склад списку             *)
       (forall lst, sorted (f lst)) }.    (* та утворює відсортований список   *)
 
@@ -89,6 +88,8 @@ Fixpoint aux_ins_sort (n : nat) (lst : list nat) : list nat :=
   | m :: lst' => if le_gt_dec n m then n :: m :: lst'
                  else m :: (aux_ins_sort n lst')
   end.
+Eval simpl in aux_ins_sort 3 [1; 4; 2; 5].
+Eval simpl in aux_ins_sort 3 [1; 3; 4; 2; 5].
 
 Lemma aux_ins_sort_same : forall n lst, same (aux_ins_sort n lst) (n :: lst).
 (* вставка числа у список в голову і за допомогою функції 'aux_ins_sort'
@@ -102,8 +103,7 @@ Proof.
     + reflexivity.
     + assert (occnum k (n :: m :: lst') = occnum k (m :: n :: lst')). {
         rewrite same_permutation with lst' lst' _ _ _; auto with sortHDB. }
-      rewrite H.
-      now apply same_cons.
+      rewrite H. now apply same_cons.
 Qed.
 
 Lemma gt_le : forall n m, n > m -> m <= n.
@@ -123,7 +123,7 @@ Proof.
     assumption || constructor || now apply gt_le.
   - intros m k lst' H1 H2 H3. 
     destruct (le_gt_dec n m); simpl.
-    + repeat constructor; try assumption.
+    + repeat constructor; assumption.
     + destruct (le_gt_dec n k); simpl; constructor; try assumption.
       now apply gt_le.
 Qed.
@@ -136,7 +136,7 @@ Fixpoint ins_sort (lst : list nat) : list nat :=
   end.
 Eval simpl in ins_sort [5; 4; 3; 2; 1].
 
-Theorem ins_sort_certified : SortSpec.
+Theorem ins_sort_certified : SortCert.
 (* сертифікована функція сортування вставкою                                  *)
 Proof. 
   exists ins_sort.
